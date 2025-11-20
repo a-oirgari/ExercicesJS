010a4059b2353f1aa9166ce2cfef7794bd82f470
a-oirgari <abdrahmanghali1998@gmail.com>
2025-11-20 22:24:03 +0100

    exercises

--- easy/easy1.js
+++ easy/easy1.js
@@ -1,69 +1,80 @@
-// ===============firsteasy==========================================
+// // ===============firsteasy==========================================
 import { employees } from "../dataset.js"; 
 
 
-function getFirstEmployeeFirstName(employees) {
-  return employees[0].firstName;
+// function getFirstEmployeeFirstName(employees) {
+//   return employees[0].firstName;
 
-}
-console.log(getFirstEmployeeFirstName(employees));
-// ===================secondeeasy====================================
+// }
+// console.log(getFirstEmployeeFirstName(employees));
+// // ===================secondeeasy====================================
 
-function getLastEmployeeLastName(employees) {
-  return employees[employees.length -  1].lastName;
-}
-console.log(getLastEmployeeLastName(employees));
+// function getLastEmployeeLastName(employees) {
+//   return employees[employees.length -  1].lastName;
+// }
+// console.log(getLastEmployeeLastName(employees));
 
-// ==================================3easy==============================
-function getEmployeeDepartmentById(employees, id) {
-  for (let i = 0; i < employees.length - 1; i++){
-    if (employees[i].id === 5){
-      return employees[i].department;
-    }
-  }
-}
-console.log(getEmployeeDepartmentById(employees, 5));
+// // ==================================3easy==============================
+// function getEmployeeDepartmentById(employees, id) {
+//   for (let i = 0; i < employees.length - 1; i++){
+//     if (employees[i].id === 5){
+//       return employees[i].department;
+//     }
+//   }
+// }
+// console.log(getEmployeeDepartmentById(employees, 5));
 
-// ==============================4easy=================================
-function countEmployees(employees){
-  let  l = employees.length;
-  return l;
-}
-console.log(countEmployees(employees));
+// // ==============================4easy=================================
+// function countEmployees(employees){
+//   let  l = employees.length;
+//   return l;
+// }
+// console.log(countEmployees(employees));
 
-// ===================================================5easy==================
-function hasInactiveEmployees(employees){
-  for (let i = 0; i < employees.length - 1; i++){
-    if (!employees.isActive){
-      return true;
-    }
-  }
-}
-console.log(hasInactiveEmployees(employees));
-// =============================================6easy===========================
-function calculateAverageAge(employees){
-  let sum = 0;
-  for (let i = 0; i < employees.length - 1; i++){
-    sum += employees[i].age;
-  }
-  sum = sum/(employees.length - 1);
-  return sum;
-}
-console.log(calculateAverageAge(employees));
-// ====================================7easy================================
-function getHighestPaidEmployee(employees){
-  let sal = employees[0];
-  for (let i = 1; i < employees.length ; i++){
-    if (employees[i].salary > sal.salary){
-      sal = employees[i];
-    }
-  }
-  return sal
-}
-console.log(getHighestPaidEmployee(employees));
+// // ===================================================5easy==================
+// function hasInactiveEmployees(employees){
+//   for (let i = 0; i < employees.length - 1; i++){
+//     if (!employees.isActive){
+//       return true;
+//     }
+//   }
+// }
+// console.log(hasInactiveEmployees(employees));
+// // =============================================6easy===========================
+// function calculateAverageAge(employees){
+//   let sum = 0;
+//   for (let i = 0; i < employees.length - 1; i++){
+//     sum += employees[i].age;
+//   }
+//   sum = sum/(employees.length - 1);
+//   return sum;
+// }
+// console.log(calculateAverageAge(employees));
+// // ====================================7easy================================
+// function getHighestPaidEmployee(employees){
+//   let sal = employees[0];
+//   for (let i = 1; i < employees.length ; i++){
+//     if (employees[i].salary > sal.salary){
+//       sal = employees[i];
+//     }
+//   }
+//   return sal
+// }
+// console.log(getHighestPaidEmployee(employees));
 // =======================================8easy===========================
+function countEmployeesInDepartment(employees, department) {
+  let num = 0;
 
+  for (let i = 0; i < employees.length; i++) {
+    if (employees[i].department === department) {
+      num++;
+    }
+  }
 
+  return num;
+}
+
+console.log(countEmployeesInDepartment(employees, "Développement"));
 
 
 
@@ -105,3 +116,15 @@ function getUniqueDepartments(employees) {
   return departments;
 }
 console.log(getUniqueDepartments(employees));
+
+
+// ==============================10easy=====================
+function hasEmployeesWithoutProjects(employees){
+  for (let i = 0; i < employees.length; i++){
+    if (!employees[i].projects || employees[i].projects.length === 0){
+      return true;
+    }
+  }
+  return false;
+}
+console.log(hasEmployeesWithoutProjects(employees));
---
+++ medium/medium1.js
@@ -0,0 +1,55 @@
+import { employees } from "../dataset.js"; 
+// ======================medium1=================
+// function ountEnglishSpeakingEmployees(employees){
+//     let num = 0;
+//     for (let i = 0; i < employees.length; i++){
+//         if ((employees[i].languages[1] || employees[i].languages[2]) === "English"){
+//         num++;
+//         }
+//     }
+//     return num;
+// }
+// console.log(ountEnglishSpeakingEmployees(employees))
+
+// // ==============================medium11====================
+// function getEmployeesByProject(employees, project){
+//     // let num = 0;
+//     const projet = [];
+//     for (let i = 0; i < employees.length; i++){
+//         if ((employees[i].projects[1] || employees[i].projects[2]) === project){
+//            projet.push(employees[i])
+//         }
+//     }
+//     //  return Object.entries(projet);
+//     return projet
+// }
+// console.log(getEmployeesByProject(employees, "Project Gamma"))
+// ==================================13medium===========================
+// function getEmployeesWithMoreThanTwoSkills(employees){
+//     const skills_emp = [];
+//     for (let i = 0; i < employees.length; i++){
+//         if (employees[i].skills.length >= 2){
+//            skills_emp.push(employees[i]);
+//         }
+//     }
+//     return skills_emp;
+// }
+// console.log(getEmployeesWithMoreThanTwoSkills(employees));
+// ======================================14medium=======================
+// function getEmployeesByLastNameInitia(employees){
+//     let names = [];
+//     for (let i = 0; i < employees.length; i++){
+//         let l ="L"
+//         if (employees[i].lastName[0] === l){
+//            names.push(employees[i]);
+//         }
+
+//     }
+//     return names;
+
+// }
+// console.log(getEmployeesByLastNameInitia(employees));
+// ====================================15medium================
+
+
+
